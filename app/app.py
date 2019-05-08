147eb194423c5d07b177a1d0179f11ca544ebd02
Ismaellov <ismael.venturadasilva0@gmail.com>
2019-05-08 00:55:25 -0300

    Adicionando docstring nas rotas, classe e funções criadas, e melhorando template.

--- app/app.py
+++ app/app.py
@@ -1,3 +1,6 @@
+
+u"""Formulário web utilizando Flask e WTForms."""
+
 from flask import Flask, render_template, request
 from wtforms import Form, PasswordField, StringField, SubmitField
 
@@ -5,6 +8,7 @@ app = Flask(__name__)
 
 
 class Login(Form):
+    u"""Classe para montar o formulário."""
     login = StringField()
     password = PasswordField()
     btn = SubmitField('Logar', )
@@ -12,15 +16,18 @@ class Login(Form):
 
 @app.route('/')
 def home():
+    u"""Rota inicial, exibe o template do formulário."""
     return render_template('login.html', form=Login())
 
 
 @app.route('/check_login', methods=['POST'])
 def check_login():
+    u"""Rota para validar dados do formulário."""
     if validate_login(request.form['login'], request.form['password']):
         return 'Logado com sucesso!!'
     return 'Usuário ou senha inválido'
 
 
 def validate_login(user, senha):
+    u"""Função de validação dos dados do formulário."""
     return user == 'teste' and senha == '123'
