f9d294961d26eb84016f4d359ca691af1f5f8f58
ismael.silva <ismael.silva@gastecnologia.com.br>
2019-05-09 13:41:56 -0300

    Arrumando rota que valida o cadastro, criada a classe de cadastro, arrumando docstring e adicionando legenda nos formulários.

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 # coding: utf-8
-u"""Formulário web utilizando Flask e WTForms."""
+"""Formulário web utilizando Flask e WTForms."""
 
 from flask import Flask, render_template, request
+
 from wtforms import Form, PasswordField, StringField, SubmitField
 
 app = Flask(__name__)
@@ -9,41 +10,51 @@ users = {}
 
 
 class Login(Form):
-    u"""Classe para montar o formulário."""
+    """Classe para montar o formulário."""
 
     login = StringField()
     password = PasswordField()
-    btn = SubmitField('Logar', )
+    btn = SubmitField('Logar')
+
+
+class Cadastro(Form):
+    """Classe para montar o formulário de cadastro."""
+
+    login = StringField()
+    password = PasswordField()
+    btn = SubmitField('Logar')
 
 
 @app.route('/login')
 def login():
-    u"""Rota inicial, exibe o template do formulário."""
+    """Rota inicial, exibe o template do formulário."""
     return render_template('login.html', form=Login())
 
 
 @app.route('/check_login', methods=['POST'])
 def check_login():
-    u"""Rota para validar dados do formulário."""
+    """Rota para validar dados do formulário."""
     if validate_login(request.form['login'], request.form['password']):
         return 'Logado com sucesso!!'
     return 'Usuário ou senha inválido'
 
 
 def validate_login(user, senha):
-    u"""Função de validação dos dados do formulário."""
+    """Função de validação dos dados do formulário."""
     return user in users and senha == users[user]
 
 
 @app.route('/')
 def home():
-    return render_template('cadastro.html', form=Login())
+    """Rota inicial, com formulário para cadastro."""
+    return render_template('cadastro.html', form=Cadastro())
 
 
-@app.route('/checar_cadastro')
+@app.route('/checar_cadastro', methods=['POST'])
 def checar_cadastro():
+    """Rota para checar cadastro."""
     if request.form['login'] not in users:
         users.update({request.form['login']: request.form['password']})
-        return 'Deu bom'
+        return render_template('login.html', form=Login())
     else:
         return 'Usuário já existente'
